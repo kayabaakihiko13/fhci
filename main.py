e9adad9af8c2a9d2c248ccd6db76eb664a38206d
kayabaakihiko13 <iqbalramad75@gmail.com>
2024-03-26 20:47:37 +0700

    feat: from function to be class package

--- main.py
+++ main.py
@@ -7,84 +7,90 @@ from bs4 import BeautifulSoup
 
 warnings.filterwarnings('ignore')
 
-URL_BASE = 'https://rekrutmenbersama2024.fhcibumn.id'
-URL_JOB = f'{URL_BASE}/job'
-URL_LOAD_RECORD = f'{URL_BASE}/job/loadRecord'
-URL_GET_DETAIL = f'{URL_BASE}/job/get_detail_vac'
+class RekrutmenBersama(object):
+    def __init__(self) -> None:
+        self.url_base ='https://rekrutmenbersama2024.fhcibumn.id'
+        self.url_job = f'{self.url_base}/job'
+        self.url_load_record = f'{self.url_base}/job/loadRecord'
+        self.url_get_detail = f'{self.url_base}/job/get_detail_vac'
 
-headers = {
-    'User-Agent' : 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Mobile Safari/537.36'
-}
+    def requests_all_job(self, to_csv: bool = False, path_save: str = None):
+        session = requests.Session()
+        soupjob = BeautifulSoup(session.get(self.url_job).content, 'html.parser')
+        csrftoken = soupjob.find('input', dict(name='csrf_fhci'))['value']
+        print(csrftoken)
+        jobs = session.post(
+            self.url_load_record,
+            data=dict(csrf_fhci=csrftoken, company='all'),
+            verify=False
+        )
+        print(jobs.status_code)
+        if to_csv and path_save:
+            self.__parse_to_csv(jobs.json()['data']['result'], path_save)
+        else:
+            return jobs.json()
 
+    def __parse_to_csv(self, data, path):
+        keys = data[0].keys()
+        with open(path, 'w', newline='',encoding="utf-8") as f:
+            dict_writer = csv.DictWriter(f, keys)
+            dict_writer.writeheader()
+            dict_writer.writerows(data)
+            for row in data:
+                clean_row = {}
+                for key, value in row.items():
+                    if value is not None:
+                        clean_row[key] = BeautifulSoup(value, 'html.parser').get_text()
+                    else:
+                        clean_row[key] = None
+                dict_writer.writerow(clean_row)
 
-def requests_all_job():
-    session = requests.Session()
-    soupjob = BeautifulSoup(session.get(URL_JOB, headers=headers, verify=False).content)
-    csrftoken = soupjob.find('input', dict(name='csrf_fhci'))['value']
-    print(csrftoken)
-    jobs = session.post(
-        URL_LOAD_RECORD,
-        data=dict(csrf_fhci=csrftoken, company='all'),
-        headers=headers,
-        verify=False
-    )
-    print(jobs.status_code)
-    return jobs.json()
+    def __get_detail_jobs(self, job_id):
+        session = requests.Session()
+        soupjob = BeautifulSoup(session.get(self.url_job).content, 'html.parser')
+        csrftoken = soupjob.find('input', dict(name='csrf_fhci'))['value']
+        detail = session.post(
+            self.url_get_detail,
+            data=dict(csrf_fhci=csrftoken, id=job_id),
+            verify=False
+        )
+        print(detail.status_code)
+        print(detail.text)
+        if detail.status_code == 200:
+            return detail.json()
+        return None
 
-
-def parse_to_csv(data, path):
-    keys = data[0].keys()
-    with open(path, 'w', newline='') as f:
-        dict_writer = csv.DictWriter(f, keys)
-        dict_writer.writeheader()
-        dict_writer.writerows(data)
-
-
-def get_detail_jobs(job_id):
-    session = requests.Session()
-    soupjob = BeautifulSoup(session.get(URL_JOB, headers=headers, verify=False).content)
-    csrftoken = soupjob.find('input', dict(name='csrf_fhci'))['value']
-    detail = session.post(
-        URL_GET_DETAIL,
-        data=dict(csrf_fhci=csrftoken, id=job_id),
-        headers=headers,
-        verify=False
-    )
-    print(detail.status_code)
-    print(detail.text)
-    if detail.status_code == 200:
-        return detail.json()
-    return 0
-
-
-def get_all_details(path):
-    vacant_ids = []
-    with open(path, 'r') as f:
-        reader = csv.reader(f, delimiter=",")
-        for i, row in enumerate(reader):
-            if i == 0:
+    def get_all_details(self,path_job_csv, to_csv: bool = False, path_save: str = None):
+        vacant_ids = []
+        with open(path_job_csv, 'r') as f:
+            reader = csv.reader(f, delimiter=",")
+            for i, row in enumerate(reader):
+                if i == 0:
+                    continue
+                vacant_ids.append(row[0])
+        print(len(vacant_ids))
+        data = []
+        for id in tqdm.tqdm(vacant_ids):
+            try:
+                datum = self.__get_detail_jobs(id)
+                if datum:
+                    data.append(datum)
+                    time.sleep(0.1)
+            except Exception as e:
+                print("Error:", e)
                 continue
-            vacant_ids.append(row[0])
-    print(len(vacant_ids))
-    data = []
-    for id in tqdm.tqdm(vacant_ids):
-        try:
-            datum = get_detail_jobs(id)
-            if datum == 0:
-                continue
-            data.append(datum)
-            time.sleep(0.1)
-        except Exception as e:
-            print("Err.. ", e)
-            continue
-    return data
+        
+        # Check if data is empty before parsing to CSV
+        if data:
+            if to_csv and path_save:
+                self.__parse_to_csv(data, path_save)
+            else:
+                return data
+        else:
+            print("No data to parse.")
 
 
 if __name__ == '__main__':
-    jobs = requests_all_job()
-    print(len(jobs['data']['result']))
-    parse_to_csv(jobs['data']['result'], 'data/all_jobs.csv')
-    print("Sleeping...")
-    time.sleep(5)
-    details = get_all_details('data/all_jobs.csv')
-    parse_to_csv(details, 'data/details.csv')
+    api_bumn = RekrutmenBersama()
+    
+    api_bumn.get_all_details(to_csv=True,path_job_csv="data/all_jobs.csv", path_save="data/all_detail.csv")
